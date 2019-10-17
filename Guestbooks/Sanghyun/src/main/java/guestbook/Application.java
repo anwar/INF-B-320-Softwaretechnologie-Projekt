c38712dd5a8b3f7e40fd42540fbb04b91505b419
sanghyun <sanghyun6339@gmail.com>
2019-10-17 18:14:29 +0200

    Guestbook mit Email

--- Guestbooks/Sanghyun/src/main/java/guestbook/Application.java
+++ Guestbooks/Sanghyun/src/main/java/guestbook/Application.java
@@ -60,12 +60,12 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
-					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
-					new GuestbookEntry("Duke Nukem", "It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
-					new GuestbookEntry("Gump1337",
-							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
-					.forEach(guestbook::save);
+				new GuestbookEntry("H4xx0r", "first!!!", "abcd@gmail.com"), //
+				new GuestbookEntry("Arni", "Hasta la vista, baby", "12345abc@yahoo.de"), //
+				new GuestbookEntry("Duke Nukem", "It's time to kick ass and chew bubble gum. And I'm all out of gum.","abcd1234@gmail.com"), //
+				new GuestbookEntry("Gump1337",
+					"Mama always said life was like a box of chocolates. You never know what you're gonna get.","qwer123@gmail.com")) //
+				.forEach(guestbook::save);
 		};
 	}
 
