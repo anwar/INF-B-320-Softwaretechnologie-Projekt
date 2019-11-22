a708fc315945d660eee49f9a89ff5aa0b36427d9
FrancyLeik <franziska.kiel@gmx.net>
2019-11-22 20:41:45 +0100

    added login html + application, modified some things

--- src/main/java/kickstart/Application.java
+++ src/main/java/kickstart/Application.java
@@ -20,22 +20,43 @@ import org.salespointframework.SalespointSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSalespoint
 public class Application {
+
+	private static final String LOGIN_ROUTE = "/login";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Configuration
+	static class KleingartenConfiguration implements WebMvcConfigurer {
+
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController(LOGIN_ROUTE).setViewName("login");
+			registry.addViewController("/").setViewName("index");
+		}
+	}
+
+	@Configuration
 	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
+
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().antMatchers("/**").permitAll().and()
-					.formLogin().loginProcessingUrl("/login").and()
-					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/**").permitAll().and().//
+				formLogin().loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE).and(). //
+				logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		}
 	}
 }
+
+
