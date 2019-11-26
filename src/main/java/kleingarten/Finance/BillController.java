b51fd3fbe4ca3ab96b1b7fbf20fbf80c55836cc7
sanghyun <sanghyun6339@gmail.com>
2019-11-26 21:44:55 +0100

    add bill.html

--- src/main/java/kleingarten/Finance/BillController.java
+++ src/main/java/kleingarten/Finance/BillController.java
@@ -2,6 +2,7 @@ package kleingarten.Finance;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BillController {
@@ -11,5 +12,9 @@ public class BillController {
 
 	}
 
+	@GetMapping("/bill")
+	String viewBill(){
+		return "bill";
+	}
 
 }
