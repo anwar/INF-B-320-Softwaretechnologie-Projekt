eb57be6282e6ac9ac6bdf4febab544b24a49e7b0
Ylvi Bachmann <ylvi@ylja.de>
2019-11-26 10:01:01 +0100

    GH-33 [WIP] Implement class Plot 36min

--- src/test/java/kleingarten/plot/PlotTest.java
+++ src/test/java/kleingarten/plot/PlotTest.java
@@ -4,7 +4,10 @@ import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import javax.money.MonetaryAmount;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.salespointframework.core.Currencies.EURO;
 
 public class PlotTest {
@@ -25,5 +28,83 @@ public class PlotTest {
 		assertThat(plot.getElectricityCountDifference()).isEqualTo(0);
 		assertThat(plot.getWaterCount()).isEqualTo(6);
 		assertThat(plot.getWaterCountDifference()).isEqualTo(0);
+	}
+
+	@Test
+	public void testChanges() {
+		plot.setStatus(PlotStatus.TAKEN);
+		plot.setSize(300);
+		plot.setDescription("This plot is rented");
+		plot.setEstimator(Money.of(400, EURO));
+		plot.setElectricityCountDifference(120.5);
+		plot.setWaterCountDifference(200.5);
+
+		assertThat(plot.getStatus()).isEqualTo(PlotStatus.TAKEN);
+		assertThat(plot.getSize()).isEqualTo(300);
+		assertThat(plot.getDescription()).isEqualTo("This plot is rented");
+		assertThat(plot.getEstimator()).isEqualTo(Money.of(400, EURO));
+		assertThat(plot.getElectricityCount()).isEqualTo(120.5);
+		assertThat(plot.getElectricityCountDifference()).isEqualTo(115.5);
+		assertThat(plot.getWaterCount()).isEqualTo(200.5);
+		assertThat(plot.getWaterCountDifference()).isEqualTo(194.5);
+	}
+
+	@Test
+	public void testIllegalArguments() {
+		PlotStatus status = null;
+		int size_negative = -5;
+		int size = 0;
+		String description = null;
+		MonetaryAmount estimator_negative = Money.of(-5, EURO);
+		MonetaryAmount estimator = null;
+		double electricityCount_negative = -2.5;
+		double electricityCount = 0;
+		double electricityCount_sinks = plot.getElectricityCount() - 2;
+		double waterCount_negative = -2.5;
+		double waterCount = 0;
+		double waterCount_sinks = plot.getWaterCount() - 2;
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setStatus(status);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setSize(size_negative);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setSize(size);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setDescription(description);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setEstimator(estimator_negative);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setEstimator(estimator);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setElectricityCountDifference(electricityCount_negative);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setElectricityCountDifference(electricityCount_sinks);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setElectricityCountDifference(electricityCount);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setElectricityCount(electricityCount_negative);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setWaterCountDifference(waterCount_negative);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setWaterCountDifference(waterCount_sinks);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setWaterCountDifference(waterCount);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			plot.setWaterCount(waterCount_negative);
+		});
 	}
 }
