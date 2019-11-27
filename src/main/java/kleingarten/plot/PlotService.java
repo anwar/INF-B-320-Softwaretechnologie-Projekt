c9ca7701a82746af55d059ffae61e18b2e128da0
Ylvi Bachmann <ylvi@ylja.de>
2019-11-27 22:49:33 +0100

    GH-45 Add dataInitializer for plots 0h 49m

--- src/main/java/kleingarten/plot/PlotService.java
+++ src/main/java/kleingarten/plot/PlotService.java
@@ -1,24 +1,47 @@
 package kleingarten.plot;
 
 import jdk.jshell.spi.ExecutionControl;
+import kleingarten.Finance.ProcedureManager;
 import kleingarten.tenant.Tenant;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import javax.money.MonetaryAmount;
 import java.time.Year;
 
 @Component
 public class PlotService {
-	private PlotCatalog plotCatalog;
+	private final PlotCatalog plotCatalog;
+	private final ProcedureManager procedureManager;
+
+	PlotService(PlotCatalog plotCatalog, ProcedureManager procedureManager){
+
+		Assert.notNull(plotCatalog, "PlotCatalog must not be null!");
+		Assert.notNull(procedureManager, "ProcedureManager must not be null!");
+		this.plotCatalog = plotCatalog;
+		this.procedureManager = procedureManager;
+	}
 
 	/**
-	 * Greates a new object of type {@link Plot} and adds it to the {@link PlotCatalog}
+	 * Creates a new object of type {@link Plot} and adds it to the {@link PlotCatalog}
 	 * @param size size of the {@link Plot} as int
 	 * @param description description of the {@link Plot} as String
 	 */
-	void addNewPlot(int size, String description) throws ExecutionControl.NotImplementedException {
-		throw new ExecutionControl.NotImplementedException("Not implemented jet!");
+	public Plot addNewPlot(String name, int size, String description) {
+		if (this.existsByName(name)) {
+			throw new IllegalArgumentException("Plot with the given name exists already!");
+		}
+		return this.plotCatalog.save(new Plot(name, size, description));
+	}
+
+	/**
+	 * Checks if there is a {@link Plot} with the given name in the {@link PlotCatalog}
+	 * @param name name of the {@link Plot} as String
+	 * @return true, if {@link Plot} with the given name exists
+	 */
+	public boolean existsByName(String name) {
+		return !this.plotCatalog.findByName(name).isEmpty();
 	}
 
 	/**
@@ -27,7 +50,7 @@ public class PlotService {
 	 * @param description new description of {@link Plot} as String
 	 * @param estimator new estimator of {@link Plot} of type {@link MonetaryAmount}
 	 */
-	void updatePlotInfo(Plot associatedPlot, String description, MonetaryAmount estimator) throws ExecutionControl.NotImplementedException {
+	public void updatePlotInfo(Plot associatedPlot, String description, MonetaryAmount estimator) throws ExecutionControl.NotImplementedException {
 		throw new ExecutionControl.NotImplementedException("Not implemented jet!");
 	}
 
@@ -35,7 +58,7 @@ public class PlotService {
 	 * Getter for all {@link Plot}s which are not rented
 	 * @return free {@link Plot}s as {@link Streamable} of type {@link Plot}
 	 */
-	Streamable<Plot> getFreePlots() throws ExecutionControl.NotImplementedException {
+	public Streamable<Plot> getFreePlots() throws ExecutionControl.NotImplementedException {
 		throw new ExecutionControl.NotImplementedException("Not implemented jet!");
 	}
 
