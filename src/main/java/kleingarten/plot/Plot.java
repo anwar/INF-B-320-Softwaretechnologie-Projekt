31781e0339b5218152eed98178f4f6a4bcd53a51
Ylvi Bachmann <ylvi@ylja.de>
2019-11-27 15:55:53 +0100

    GH-39 [WIP] Use class procedure to save counters

--- src/main/java/kleingarten/plot/Plot.java
+++ src/main/java/kleingarten/plot/Plot.java
@@ -12,14 +12,8 @@ public class Plot extends Product {
 	private int size;
 	private String description;
 
-	//ElectricityCount and WaterCount have duplicates to save the value of the counter and the difference since the year before
-	private double electricityCount = -1;
-	private double electricityCountDifference;
-	private double waterCount = -1;
-	private double waterCountDifference;
-
 	/**
-	 * Private constructor of class Plot, which is used by the Spring Framework
+	 * Private constructor of class {@link Plot}, which is used by the Spring Framework
 	 */
 	private Plot() {
 		this.status = null;
@@ -27,25 +21,21 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Constructor of class Plot
-	 * @param name number to identify the plot as String to be easy readable by the user
-	 * @param price estimator as MonetaryAmount, that represents the value of the plot
-	 * @param size size of the plot as int
-	 * @param description String that describes the look of a plot
-	 * @param electricityCount value of the electric meter of a plot as double
-	 * @param waterCount value of the water meter of a plot as double
+	 * Constructor of class {@link Plot}
+	 * @param name number to identify the {@link Plot} as String to be easy readable by the user
+	 * @param price estimator as {@link MonetaryAmount}, that represents the value of the {@link Plot}
+	 * @param size size of the {@link Plot} as int
+	 * @param description String that describes the look of a {@link Plot}
 	 */
-	public Plot(String name, MonetaryAmount price, int size, String description, double electricityCount, double waterCount) {
+	public Plot(String name, MonetaryAmount price, int size, String description) {
 		super(name, price);
 		this.status = PlotStatus.FREE;
 		this.size = size;
 		this.description = description;
-		this.electricityCount = electricityCount;
-		this.waterCount = waterCount;
 	}
 
 	/**
-	 * Getter for the status of a plot
+	 * Getter for the status of a {@link Plot}
 	 * @return status as Enum of type {@link PlotStatus}
 	 */
 	public PlotStatus getStatus() {
@@ -53,7 +43,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Setter for the status of a plot
+	 * Setter for the status of a {@link Plot}
 	 * @param status status as {@link PlotStatus}
 	 */
 	public void setStatus(PlotStatus status) {
@@ -64,7 +54,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Getter for the size of a plot
+	 * Getter for the size of a {@link Plot}
 	 * @return size as int
 	 */
 	public int getSize() {
@@ -72,7 +62,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Setter for the size of a plot
+	 * Setter for the size of a {@link Plot}
 	 * @param size size as int
 	 */
 	public void setSize(int size) {
@@ -83,7 +73,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Getter for the description of a plot
+	 * Getter for the description of a {@link Plot}
 	 * @return description as String
 	 */
 	public String getDescription() {
@@ -91,7 +81,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Setter for the description of a plot
+	 * Setter for the description of a {@link Plot}
 	 * @param description description as String
 	 */
 	public void setDescription(String description) {
@@ -102,7 +92,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Getter for the estimator (or price) of a plot
+	 * Getter for the estimator (or price) of a {@link Plot}
 	 * @return estimator as {@link MonetaryAmount}
 	 */
 	public MonetaryAmount getEstimator() {
@@ -110,7 +100,7 @@ public class Plot extends Product {
 	}
 
 	/**
-	 * Setter for the estimator (or price) of a plot
+	 * Setter for the estimator (or price) of a {@link Plot}
 	 * @param estimator estimator as {@link MonetaryAmount}
 	 */
 	public void setEstimator(MonetaryAmount estimator) {
@@ -121,83 +111,5 @@ public class Plot extends Product {
 			throw new IllegalArgumentException("Estimator must be positive!");
 		}
 		this.setPrice(estimator);
-	}
-
-	/**
-	 * Getter for the electricityCount (value of the electric meter) of a plot
-	 * @return electricityCount as double
-	 */
-	public double getElectricityCount() {
-		return electricityCount;
-	}
-
-	/**
-	 * Setter for the electricityCount (value of the electric meter) of a plot
-	 * @param electricityCount electricityCount as double
-	 */
-	public void setElectricityCount(double electricityCount) {
-		if (electricityCount < 0) {
-			throw new IllegalArgumentException("ElectricityCount must not be negative!");
-		}
-		this.electricityCount = electricityCount;
-	}
-
-	/**
-	 * Getter for the electricityCountDifference (difference of the electric meter value since the last year) of a plot
-	 * @return electricityCountDifference as double
-	 */
-	public double getElectricityCountDifference() {
-		return electricityCountDifference;
-	}
-
-	/**
-	 * Setter for the electricityCountDifference (difference of the electric meter value since the last year) of a plot
-	 * @param electricityCount value of the electric meter of this year as double
-	 */
-	public void setElectricityCountDifference(double electricityCount) {
-		if (electricityCount < this.electricityCount) {
-			throw new IllegalArgumentException("ElectricityCount must not be negative!");
-		}
-		this.electricityCountDifference = electricityCount - this.electricityCount;
-		this.setElectricityCount(electricityCount);
-	}
-
-	/**
-	 * Getter for the waterCount (value of the water meter) of a plot
-	 * @return waterCount as double
-	 */
-	public double getWaterCount() {
-		return waterCount;
-	}
-
-	/**
-	 * Setter for the waterCount (value of the water meter) of a plot
-	 * @param waterCount waterCount as double
-	 */
-	public void setWaterCount(double waterCount) {
-		if (waterCount < 0) {
-			throw new IllegalArgumentException("WaterCount must not be negative!");
-		}
-		this.waterCount = waterCount;
-	}
-
-	/**
-	 * Getter for the waterCountDifference (difference of the water meter value since the last year) of a plot
-	 * @return waterCountDifference as double
-	 */
-	public double getWaterCountDifference() {
-		return waterCountDifference;
-	}
-
-	/**
-	 * Setter for the waterCountDifference (difference of the water meter value since the last year) of a plot
-	 * @param waterCount value of the water meter of this year as double
-	 */
-	public void setWaterCountDifference(double waterCount) {
-		if (waterCount < this.waterCount) {
-			throw new IllegalArgumentException("WaterCount must not be negative!");
-		}
-		this.waterCountDifference = waterCount - this.waterCount;
-		this.setWaterCount(waterCount);
 	}
 }
