d6c254fb863c6bfedead6bea64a77aa147a4bcb3
FrancyLeik <franziska.kiel@gmx.net>
2019-11-29 18:46:57 +0100

    tenantdetails.html merge

--- src/test/java/kleingarten/Finance/ProcedureManagerTest.java
+++ src/test/java/kleingarten/Finance/ProcedureManagerTest.java
@@ -3,17 +3,17 @@ package kleingarten.Finance;
 
 import static org.assertj.core.api.Assertions.*;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import kleingarten.plot.Plot;
 
 // https://www.baeldung.com/spring-boot-testing
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 public class ProcedureManagerTest {
 
 	@Autowired
@@ -23,7 +23,7 @@ public class ProcedureManagerTest {
 	private ProcedureManager procedureManager;
 
 
-	//@Test
+	@Test
 	public void findProcedureByIdTest() {
 		Plot plot = new Plot("sandkasten", 10, "Vorsicht: Kinder spielen hier.");
 		Procedure p1 = new Procedure( 2018, plot, 1l );
