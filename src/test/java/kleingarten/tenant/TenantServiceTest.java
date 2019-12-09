6022a16753f0acf61f9e1e57b63820ecd19e8c3f
isaahzorgh <isabell.zorr@gmx.de>
2019-12-09 23:24:47 +0100

    test for changing email now properly working

--- src/test/java/kleingarten/tenant/TenantServiceTest.java
+++ src/test/java/kleingarten/tenant/TenantServiceTest.java
@@ -3,6 +3,7 @@ package kleingarten.tenant;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.salespointframework.useraccount.AuthenticationManager;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.Role;
 import org.salespointframework.useraccount.UserAccountManager;
@@ -10,25 +11,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.*;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
 public class TenantServiceTest {
 	private final UserAccountManager userAccountManager;
 	private final TenantService tenantService;
+	private final TenantRepository tenantRepository;
+	private final TenantManager tenantManager;
 
 	private Tenant tenant;
 
-	public TenantServiceTest(@Autowired UserAccountManager userAccountManager, @Autowired TenantService tenantService) {
+	//TenantService(TenantManager tenantManager, UserAccountManager userAccountManager, TenantRepository tenants, AuthenticationManager authenticationManager)
+	public TenantServiceTest(@Autowired UserAccountManager userAccountManager, @Autowired TenantRepository tenantRepository, @Autowired AuthenticationManager authenticationManager) {
 		this.userAccountManager = userAccountManager;
-		this.tenantService = tenantService;
+		this.tenantRepository = tenantRepository;
+		this.tenantManager = new TenantManager(tenantRepository, userAccountManager);
+		this.tenantService = new TenantService(tenantManager, userAccountManager, tenantRepository, authenticationManager);
 	}
 
 	@BeforeEach
 	void SetUp(){
-		tenant = new Tenant("Jassi", "Gepackert", "Neben Isa und Francy",
-			"908964875734", "13.05.1999", userAccountManager.create("jassi", Password.UnencryptedPassword.of("123"),"jassis@email.com", Role.of("Hauptpächter")));
-		tenant.setId(Long.parseLong("1"));
+
+//		tenant = new Tenant("Jassi", "Gepackert", "Neben Isa und Francy",
+//				"908964875734", "13.05.1999", userAccountManager.create("jassi", Password.UnencryptedPassword.of("123"),"jassis@email.com", Role.of("Hauptpächter")));
+//		tenant.setId(Long.valueOf(1000));
+//		tenantRepository.saveAll(List.of(tenant));
+//		System.out.println(tenant);
+//		System.out.println(tenantRepository.findAll().toList().get(0).getEmail());
+//		System.out.println(tenant.getId());
+//		System.out.println(tenantRepository.existsById(tenant.getId()));
+//		System.out.println(tenantRepository.existsById(Long.valueOf(1000)));
+//		System.out.println(tenantManager.get(Long.valueOf(1000)));
+//		System.out.println(tenant.getId());
 	}
 
 	@Test
@@ -40,7 +56,7 @@ public class TenantServiceTest {
 	@Test
 	void changeEmail(){
 		//assertThat(tenant.getUserAccount().getEmail().equals("jassis@email.com"));
-		tenantService.changeEmail(tenant.getId(), "jassis@email.com", "jasmins@email.com", "jasmins@email.com");
-		assertThat(tenant.getUserAccount().getEmail().equals("jasmins@email.com"));
+		tenantService.changeEmail(tenantRepository.findAll().toList().get(0).getId(), tenantRepository.findAll().toList().get(0).getEmail(), "jasmins@email.com", "jasmins@email.com");
+		assertThat(tenantRepository.findAll().toList().get(0).getEmail().equals("jasmins@email.com"));
 	}
 }
