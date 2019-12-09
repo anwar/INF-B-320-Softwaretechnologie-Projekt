b9c48eba8a65064cbf49d4e6bb7d8e5b71393106
REDLIGHTFILMS <33683591+REDLIGHTFILMS@users.noreply.github.com>
2019-12-09 13:36:37 +0100

    add function removeWorkAssignment in WorkAssignmentManager

--- src/main/java/kleingarten/appointment/WorkAssignmentManager.java
+++ src/main/java/kleingarten/appointment/WorkAssignmentManager.java
@@ -1,22 +1,10 @@
 package kleingarten.appointment;
 
 import kleingarten.plot.Plot;
-import kleingarten.plot.PlotCatalog;
-import kleingarten.plot.PlotService;
-import kleingarten.tenant.Tenant;
-import org.apache.tomcat.jni.Local;
-import org.hibernate.jdbc.Work;
-import org.salespointframework.catalog.ProductIdentifier;
-import org.salespointframework.core.SalespointIdentifier;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -24,12 +12,9 @@ public class WorkAssignmentManager {
 
 
 	private WorkAssignmentRepository workAssignmentRepository;
-	private PlotCatalog plotCatalog;
-	private PlotService plotService;
 
-	public WorkAssignmentManager(WorkAssignmentRepository workAssignmentRepository, PlotCatalog plotCatalog, PlotService plotService){
+	public WorkAssignmentManager(WorkAssignmentRepository workAssignmentRepository){
 		this.workAssignmentRepository = workAssignmentRepository;
-		this.plotService = plotService;
 	}
 
 	public List<WorkAssignment> getAll(){
@@ -71,20 +56,7 @@ public class WorkAssignmentManager {
 		return null;
 	}
 
-	public Plot findbyID(ProductIdentifier identifier){
-
-		Plot findPlotByID = plotService.findById(identifier);
-		for (Plot plot: plotCatalog.findAll()) {
-			if(plot == findPlotByID){
-				return plot;
-			}
-		}
-
-		return null;
-	}
-
-	public boolean addWorkAssignment(ProductIdentifier identifier, long workAssigmentID){
-		Plot plot = findbyID(identifier);
+	public boolean addPlotToWorkAssignment(Plot plot, long workAssigmentID){
 		WorkAssignment workAssignment = findByID(workAssigmentID);
 
 		if(!workAssignment.containsPlot(plot)){
@@ -94,21 +66,34 @@ public class WorkAssignmentManager {
 		return false;
 	}
 
-	public boolean removeWorkAssignment(ProductIdentifier identifier, long workAssigmentID){
-		Plot plot = findbyID(identifier);
+	public boolean removePlotOutWorkAssignment(Plot plot, long workAssigmentID){
 		WorkAssignment workAssignment = findByID(workAssigmentID);
 
-		if(!workAssignment.containsPlot(plot)){
+		if(workAssignment.containsPlot(plot)){
 			workAssignment.removePlot(plot);
 			return true;
 		}
 		return false;
 	}
 
+	public List<WorkAssignment> getForPlotWorkAssignments(Plot plot){
+		List<WorkAssignment> buffer = new ArrayList<>();
+		for(WorkAssignment workAssignment: workAssignmentRepository.findAll()){
+			if(workAssignment.containsPlot(plot)){
+				buffer.add(workAssignment);
+			}
+		}
+		return buffer;
+	}
+
 	public void setWorkHours(int workHours, long workAssigmentID){
 		WorkAssignment workAssignment = findByID(workAssigmentID);
 
 		workAssignment.setWorkHours(workHours);
+	}
+
+	public int getWorkAssignment(Plot plot, long workAssignmentID){
+		return 0;
 	}
 
 }
