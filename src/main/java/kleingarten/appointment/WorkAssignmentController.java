3698e44c5de831f8ffb6c0414a09b300b059d246
REDLIGHTFILMS <33683591+REDLIGHTFILMS@users.noreply.github.com>
2019-12-09 12:11:22 +0100

    1. add findByID functions in WorkAssignmentManager for WorkAssignment and Plot 2. add functions "addWorkAssignment" and "removeWorkAssignment" 3. add function "setWorkHours" for a WorkAssignment

--- src/main/java/kleingarten/appointment/WorkAssignmentController.java
+++ src/main/java/kleingarten/appointment/WorkAssignmentController.java
@@ -1,11 +1,13 @@
 package kleingarten.appointment;
 
+import org.salespointframework.catalog.ProductIdentifier;
 import org.salespointframework.core.SalespointIdentifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -17,12 +19,12 @@ public class WorkAssignmentController {
 
 	private WorkAssignmentManager workAssignmentManager;
 
-	public WorkAssignmentController(WorkAssignmentManager workAssignmentManager){
+	public WorkAssignmentController(WorkAssignmentManager workAssignmentManager) {
 		this.workAssignmentManager = workAssignmentManager;
 	}
 
 	@GetMapping("/createAssignment")
-	public String appointmentList(Model model,CreateWorkAssignmentForm form){
+	public String appointmentList(Model model, CreateWorkAssignmentForm form) {
 		model.addAttribute("dateNow", LocalDate.now());
 		model.addAttribute("timeNow", LocalTime.now());
 		model.addAttribute("form", form);
@@ -31,7 +33,7 @@ public class WorkAssignmentController {
 
 	@PostMapping("/createAssignment")
 	public String addAppointment(@Valid CreateWorkAssignmentForm form) {
-		if(!workAssignmentManager.containsListTheDate(form.getDateTime())){
+		if (!workAssignmentManager.containsListTheDate(form.getDateTime())) {
 			workAssignmentManager.createAssignment(form);
 			System.out.println("true");
 			return "redirect:/createAssignment";
@@ -41,28 +43,22 @@ public class WorkAssignmentController {
 	}
 
 	@GetMapping("/listOfAssignments")
-	String listOfAppointments(Model model){
+	String listOfAppointments(Model model) {
 		model.addAttribute("ListOfAssignments", workAssignmentManager.getAll());
 		return "workAssignment/listOfAssignments";
 	}
 
 
-
-
-	/*
-	@GetMapping("/addAppointment/{plotID}")
-	public String appointmentList(Model model, @PathVariable SalespointIdentifier plotID){
-		model.addAttribute("ListofAppointments", workAssignmentManager.getAll());
-		//model.addAttribute("ListOfTimes", appointmentManager.getAll().get().m
-		//alle einsätze auflisten
-		return "addAppointment";
+	@GetMapping("/addWorkAssignment/{plotID}")
+	public String appointmentList(@PathVariable ProductIdentifier plotID,  Model model) {
+		model.addAttribute("ListOfAssignments", workAssignmentManager.getAll());
+		return "workAssignment/addWorkAssignment";
 	}
 
 
-	@PostMapping("/addAppointment{plotID}")
-	public String addAppointment(CreateWorkAssignmentForm form, @PathVariable SalespointIdentifier plotID){
-		workAssignmentManager.addAppointment();
+	@PostMapping("/addWorkAssignment/{plotID}/{workAssigmentID}")
+	public String addAppointment(Model model, @PathVariable ProductIdentifier plotID, @PathVariable long workAssigmentID) {
+		workAssignmentManager.addWorkAssignment(plotID, workAssigmentID);
 		return "redirect:/myPlot";
 	}
-	*/
 }
