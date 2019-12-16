f8cc1df8bdf53d5468b100ea41a0495f5b8387b6
Ylvi Bachmann <ylvi@ylja.de>
2019-12-16 12:59:42 +0100

    Add tests for PlotInformationBuffer

--- src/main/java/kleingarten/plot/UpdateChairmanForm.java
+++ src/main/java/kleingarten/plot/UpdateChairmanForm.java
@@ -2,15 +2,13 @@ package kleingarten.plot;
 
 import org.salespointframework.catalog.ProductIdentifier;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class UpdateChairmanForm {
 	private List<ProductIdentifier> updatedPlots;
 
-	public UpdateChairmanForm() {}
+	public UpdateChairmanForm() {
+	}
 
 	public UpdateChairmanForm(List<ProductIdentifier> updatedPlots) {
 		this.updatedPlots = updatedPlots;
---
+++ src/test/java/kleingarten/plot/PlotInformationBufferTests.java
@@ -0,0 +1,40 @@
+package kleingarten.plot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.money.format.MonetaryFormats;
+import java.util.HashMap;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlotInformationBufferTests {
+	private Plot plot;
+	private PlotInformationBuffer buffer;
+
+	/**
+	 * Setup of a mock of type {@link Plot} and the associated {@link PlotInformationBuffer} to run the tests with
+	 */
+	@BeforeEach
+	public void setUp() {
+		plot = new Plot("123", 500, "test");
+		buffer = new PlotInformationBuffer(plot);
+	}
+
+	/**
+	 * Test if the initial setup has worked correctly
+	 */
+	@Test
+	public void initialSetupTest() {
+		assertThat(buffer.getAssociatedPlot()).isEqualTo(plot);
+		assertThat(buffer.getPlotId()).isEqualTo(plot.getId());
+		assertThat(buffer.getPlotName()).isEqualTo(plot.getName());
+		assertThat(buffer.getPlotSize()).isEqualTo(plot.getSize());
+		assertThat(buffer.getPlotDescription()).isEqualTo(plot.getDescription());
+		assertThat(buffer.getPlotPrice()).isEqualTo(MonetaryFormats.getAmountFormat(Locale.GERMANY).format(plot.getPrice()));
+
+		assertThat(buffer.getMainTenantRoles()).isEqualTo(new HashMap<>());
+		assertThat(buffer.getSubTenantRoles()).isEqualTo(new HashMap<>());
+	}
+}
