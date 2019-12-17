ccfa60a0ed6bf754a72f7bb9a423f21df0bd3cb3
sanghyun <sanghyun6339@gmail.com>
2019-12-17 17:03:00 +0100

    work on unitTest in Finance

--- src/test/java/kleingarten/finance/ProcedureCalculationTests.java
+++ src/test/java/kleingarten/finance/ProcedureCalculationTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.Role;
 import org.salespointframework.useraccount.UserAccountManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,18 +22,29 @@ public class ProcedureCalculationTests {
 	private ProcedureManager procedureManager;
 	private PlotService plotService;
 	private Tenant tenant;
-	private Fee fee;
 	private UserAccountManager userAccountManager;
 	private TenantRepository tenantRepository;
+
 	private Plot testPlot;
 	private Procedure testProcedure;
+	private Fee fee;
+	private Fee fee1;
+	private Fee fee2;
 
-	public ProcedureCalculationTests(){
+	public ProcedureCalculationTests(@Autowired PlotService plotService, @Autowired ProcedureManager procedureManager,
+									 @Autowired UserAccountManager userAccountManager, @Autowired TenantRepository tenantRepository){
+		this.plotService = plotService;
+		this.procedureManager = procedureManager;
+		this.userAccountManager = userAccountManager;
+		this.tenantRepository = tenantRepository;
 	}
 
 	@BeforeEach
 	public void setup(){
-		fee = new Fee("test", 0, 2);
+		fee = new Fee("rent", 0, 2);
+		fee1 = new Fee("water", 0, 1);
+		fee2 = new Fee("power", 0, 1);
+
 		testPlot = plotService.addNewPlot("test", 300, "test");
 		tenant = new Tenant("tester", "test", "Test Address",
 				"123456789", "11.11.2000",
@@ -42,24 +54,64 @@ public class ProcedureCalculationTests {
 		testProcedure = procedureManager.add(new Procedure(2019, testPlot, tenant.getId()));
 	}
 
+	/**
+	 * Check Baseprice from fee
+	 */
 	@Test
 	public void initialBasePriceTest() {
 		assertThat(fee.getBasePrice()).isEqualTo(2);
 	}
 
+	/**
+	 * check whether the created/added Procedure by testPlot is same as testProcedure
+	 */
 	@Test
 	public void getProcedureTest(){
 		assertThat(procedureManager.getProcedure(2019, testPlot)).isEqualTo(testProcedure);
 	}
 
+	/**
+	 * check the plot size in testProcedure (which is created by testPlot
+	 */
 	@Test
-	public void checkProcedure(){
+	public void checkSize(){
 		assertThat(testProcedure.getSize()).isEqualTo(300);
 	}
 
+	/**
+	 * check the waterCount in testProcedure. It should be 0 because it's not given
+	 */
+	@Test
+	public void checkWater(){
+		assertThat(testProcedure.getWatercount()).isEqualTo(0);
+	}
+
+	/**
+	 * check the powerCount in testProcedure. It should be 0 because it's not given
+	 */
+	@Test
+	public void checkPower(){
+		assertThat(testProcedure.getPowercount()).isEqualTo(0);
+	}
+
+	/**
+	 * check the rent Price of the testPlot
+	 */
 	@Test
 	public void checkRentPrice(){
 		assertThat(testProcedure.getSize() * fee.getBasePrice()).isEqualTo(600);
 	}
+
+	/**
+	 * check the water price of the testPlot
+	 */
+	@Test
+	public void checkWaterPrice(){assertThat(testProcedure.getWatercount() * fee1.getBasePrice()).isEqualTo(0);}
+
+	/**
+	 * check the power price of the testPlot
+	 */
+	@Test
+	public void checkPowerPrice(){assertThat(testProcedure.getPowercount() * fee2.getBasePrice()).isEqualTo(0);}
 
 }
