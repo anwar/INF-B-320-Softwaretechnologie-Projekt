ccf16b71ac9e901b7f23b28391f0d374b796855e
isaahzorgh <isabell.zorr@gmx.de>
2020-01-16 12:43:37 +0100

    tenant tests
    issue #88

--- src/test/java/kleingarten/tenant/TenantControllerTest.java
+++ src/test/java/kleingarten/tenant/TenantControllerTest.java
@@ -6,7 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
@@ -14,11 +21,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TenantControllerTest {
 	@Autowired
 	MockMvc mvc;
+
+	@Autowired
+	TenantController tenantController;
+
+
+
 
 	@Test
 	void tenantlistIsAccessibleForManager() throws Exception {
@@ -61,11 +77,37 @@ public class TenantControllerTest {
 				.andExpect(header().string(HttpHeaders.LOCATION, endsWith("/login")));
 	}
 
-	/*@Test
+	@Test
 	void changePasswordPreventPublicAccsess()throws Exception{
-		mvc.perform(get("/changePassword").param("email", ""))
+		mvc.perform(get("/changePassword"))
 			.andExpect(status().isFound())
 			.andExpect(header().string(HttpHeaders.LOCATION, endsWith("/login")));
-	}*/
+	}
+
+
+	@Test
+	void preTenantIsAccessibleForManager() throws Exception{
+		mvc.perform(get("/pretenants").with(user("peter.klaus").roles("Vorstandsvorsitzender")))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void preTenantPreventPublicAccess() throws Exception{
+		mvc.perform(get("/pretenants"))
+				.andExpect(status().isFound())
+				.andExpect(header().string(HttpHeaders.LOCATION, endsWith("/login")));
+	}
+	@Test
+	void registerIsAccessibleForManager() throws Exception{
+		mvc.perform(get("/register").with(user("peter.klaus").roles("Vorstandsvorsitzender")))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void registerPreventPublicAccess() throws Exception{
+		mvc.perform(get("/register"))
+				.andExpect(status().isFound())
+				.andExpect(header().string(HttpHeaders.LOCATION, endsWith("/login")));
+	}
 
 }
